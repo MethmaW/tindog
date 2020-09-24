c7cabb9548b2764d8379b56253c8f22ae6ed5c6e
Mey W <mey.wjw@gmail.com>
2020-09-24 17:01:08 +0530

    add initial files

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.static("public"));
+
+
+app.get("/", function(req, res) {
+
+    res.sendFile( __dirname + "/index.html")
+  
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
+
+app.listen(port, function() {
+  console.log("Server has started successfully");
+});
